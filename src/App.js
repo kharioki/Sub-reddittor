7a31152fb498597e26a76029203a4b796c3d5379
tony <antolisah@gmail.com>
2022-02-11 02:41:41 +0300

    styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useMachine } from "@xstate/react";
+import { redditMachine } from "./state/redditMachine";
 import './App.css';
 
+const subreddits = ['reactjs', 'frontend', 'javascript'];
+
 function App() {
+  const [current, send] = useMachine(redditMachine);
+  const { subreddit, posts } = current.context;
   return (
-    <div className="App">
-      Hello!!!
-    </div>
+    <main>
+      <header>
+        <select
+          onChange={(e) => {
+            send({ type: 'SELECT', name: e.target.value });
+          }}
+        >
+          {subreddits.map((subreddit) => (
+            <option key={subreddit} value={subreddit}>
+              {subreddit}
+            </option>
+          ))}
+        </select>
+      </header>
+      <section>
+        <h1>{current.matches('idle') ? 'Select a subreddit' : subreddit}</h1>
+        {current.matches({ selected: 'loading' }) && <div>Loading...</div>}
+        {current.matches({ selected: 'loaded' }) && (
+          <ul>
+            {posts.map((post) => (
+              <li key={post.title}>{post.title}</li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </main>
   );
 }
 
